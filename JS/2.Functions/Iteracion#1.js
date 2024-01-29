19ff29584f2473cfeb49c2aa920b6ce4b9aa29d6
kanarioco <dag109.2002@gmail.com>
2024-01-29 22:57:56 +0100

    https://github.com/kanarioco/neoland-basic-algorithms.git

---
+++ JS/2.Functions/Iteracion#1.js
@@ -0,0 +1,25 @@
+/*
+**Iteración #1: Buscar el máximo**
+
+Completa la función que tomando dos números como argumento devuelva el más alto.
+
+```jsx
+function sum(numberOne, numberTwo) {
+  // insert code
+}
+```
+*/
+
+
+function sum(numberOne, numberTwo) {
+     if (numberOne > numberTwo) {
+        return numberOne;
+    }
+        else {
+        return numberTwo;
+    }
+    }
+  
+console.log(sum(31, 28));
+
+// OK
